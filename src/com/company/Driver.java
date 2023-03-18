ea3e5f9177f69737d7e04a772223495ddf9c1174
unknown <alexandar.petrov@yahoo.com>
2023-03-18 12:44:54 -0400

    source files

---
+++ src/com/company/Driver.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Driver {
+
+    public static void main(String[] args) {
+
+        Math_Functions m = new Math_Functions();
+
+        m.arccos(5);
+
+    }
+}
---
+++ src/com/company/Math_Functions.java
@@ -0,0 +1,60 @@
+package com.company;
+
+public class Math_Functions {
+
+    double arccos(double x){ //Alexandar Petrov
+
+        double answer = 2.5;
+
+        return answer;
+
+    }
+
+    double ab(double x){ //Jeremy Piperni
+
+        double answer = 2.5;
+
+        return answer;
+
+    }
+
+    double log(int b, double x){ //Dany Pham
+
+        double answer = 2.5;
+
+        return answer;
+
+    }
+
+    double MAD(double x){ //Kevin Patel
+
+        double answer = 2.5;
+
+        return answer;
+
+    }
+
+    double σ(double x){ //Benjamin Pizarro
+
+        double answer = 2.5;
+
+        return answer;
+
+    }
+
+    double sinh(double x){ //Miraj Patel
+
+        double answer = 2.5;
+
+        return answer;
+
+    }
+
+    double x(double y){ //Kirk
+
+        double answer = 2.5;
+
+        return answer;
+
+    }
+}
