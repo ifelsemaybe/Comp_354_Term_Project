b8bf724310ae91bce704d75202f9bb3224f1dc97
Kevin Patel <kevinpatel0624@gmail.com>
2023-03-24 13:24:05 -0400

    Update Driver.java
    Can modify array for anyone that needs to test functionality

--- src/com/company/Driver.java
+++ src/com/company/Driver.java
@@ -5,7 +5,9 @@ public class Driver {
     public static void main(String[] args) {
 
         Math_Functions m = new Math_Functions();
-
+        double [] x = new double[]{1,2,3,4,5,6,7,8,9,10,1.4};
+        double mad = m.MAD(x);
+        System.out.println(mad);
         m.arccos(5);
 
     }
