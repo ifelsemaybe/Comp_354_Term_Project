754365f764b3d2155e610423ccfcf00a9d0fdcd3
Kevin Patel <kevinpatel0624@gmail.com>
2023-03-24 13:21:55 -0400

    MAD function implementation
    Added additional methods to achieve cleaner code.

--- src/com/company/Math_Functions.java
+++ src/com/company/Math_Functions.java
@@ -25,13 +25,52 @@ public class Math_Functions {
         return answer;
 
     }
+    /**
+     * Calculates the mean of an array of doubles
+     * @param x
+     * @return the mean as a double
+     */
+    double meanArray(double [] x){ //Kevin Patel
+        double sum = 0;
+        //add each element of the array to the sum
+        for(int i = 0; i < x.length; i++){
+            sum += x[i];
+        }
+        //return the mean
+        return sum / x.length;
+    }
 
-    double MAD(double x){ //Kevin Patel
+    /**
+     * Returns the absolute value of the input without using Math library
+     * @param x
+     * @return absolute value as double
+     */
+    static double abs(double x){ //Kevin Patel
+        if(x < 0)
+            return -x;
+        return x;
+    }
+    /**
+     * Returns the mean absolute deviation of the input array
+     * @param x
+     * @return the mean absolute deviation as a double
+     */
+    double MAD(double [] x){ //Kevin Patel
+        double sum = 0;
+        double mean = 0;
+        //array for absolute differences
+        double [] absDiff = new double[x.length];
+        mean = meanArray(x);
 
-        double answer = 2.5;
+        //calculate absolute difference for each element
+        for(int i = 0; i < x.length; i++)
+            absDiff[i] = abs(x[i] - mean);
+        //calculate sum of the absolute differences
+        for(int i = 0; i < absDiff.length; i++)
+            sum += absDiff[i];
 
-        return answer;
-
+        //return the mean absolute deviation
+        return sum / absDiff.length;
     }
 
     double σ(double x){ //Benjamin Pizarro
