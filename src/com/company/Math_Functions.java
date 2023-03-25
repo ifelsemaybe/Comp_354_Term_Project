449169c37ab5af858f7f209301cfc66831f3431d
bpizar <80708745+bpizar@users.noreply.github.com>
2023-03-25 18:02:45 -0400

    Added stdDev (no comments)

--- src/com/company/Math_Functions.java
+++ src/com/company/Math_Functions.java
@@ -34,13 +34,35 @@ public class Math_Functions {
 
     }
 
-    double σ(double x){ //Benjamin Pizarro
+    double σ(double [] x){ //Benjamin Pizarro
+        double mean = 0;
+        double sum = 0;
+        double answer = 0;
 
-        double answer = 2.5;
+        for (int i = 0 ; i < x.length ; i++){
+        	mean = mean + x[i];
+        }
+        mean = (mean / x.length);
+        System.out.println("mean " + mean);
 
+        for (int i = 0 ; i < x.length ; i++){
+            sum = sum + ((x[i] - mean) * (x[i] - mean));
+        }
+        System.out.println("summation " + sum);
+        answer = sum / x.length;
+
+        answer = sqr(answer);
         return answer;
 
     }
+    // square root using newtons method 10 iterations
+    double sqr(double x) {
+		double x1 = 1;
+		for(int i = 1; i <= 10; i++) {
+			x1 = x1 - (((x1 * x1)- x)/ (2*x1));
+		}
+		return x1;
+	}
 
     double sinh(double x){ //Miraj Patel
 
